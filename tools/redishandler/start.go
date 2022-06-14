2827ef992e805cf4a79e94ce959d58ea0314944a
Nikola Broyak <nikola.broyak@websensa.com>
2022-06-14 20:10:51 +0200

    ddff

--- tools/redishandler/start.go
+++ tools/redishandler/start.go
@@ -9,7 +9,7 @@ var Ctx = Client.Context()
 
 func Start() *redis.Client {
 	Client := redis.NewClient(&redis.Options{
-		Addr:     "niqurl-redis-1:6379",
+		Addr:     "niqurl-redis:6379",
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
